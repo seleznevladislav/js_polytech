96d1a4c05c03e05296da30eda4824fa466066cd6
Vlad <example@gmail.com>
2022-03-17 01:08:27 +0300

    17.03.22

--- js/script3.js
+++ js/script3.js
@@ -1,26 +1,99 @@
+let inputOut = document.querySelector('#cityOut');
+let inputIn = document.querySelector('#cityIn');
+const cyrillicPattern = /^\p{Script=Cyrillic}+$/u;
+let findBtn = document.querySelector('.find');
+let time = document.querySelector('#date');
+let radioOne = document.querySelector('#Child');
+let radioTwo = document.querySelector('#Parent');
+const data = {};
 const modalForm = function(){
 	let openBtn = document.querySelector('.modal-window-btn');
 	let modalWndw = document.querySelector('.modal-window');
 	let body = document.querySelector('body');
 	let cross = document.querySelector('.cross');
-	let form = document.querySelector('form');
+	let parentDiv = document.querySelector('.parentDiv');
+
+	
 
 	openBtn.addEventListener('click', function(){
 		modalWndw.style.display = 'flex';
 		body.style.overflow = 'hidden';
-		// console.log(modalWndw);
+		parentDiv.style.width = '100%';
+		parentDiv.style.height = '100%';
+		parentDiv.style.background = 'rgba(0,0,0, 0.3)';	
 	});
 	cross.addEventListener('click', ()=>{
 		modalWndw.style.display = '';
 		body.style.overflow = '';
+		parentDiv.style.width = '';
+		parentDiv.style.height = '';
+		parentDiv.style.background = '';	
 	})
-	body.addEventListener('click', (event)=>{
-	console.log(event.target);
-	if ((event.target !== openBtn) && (event.target != modalWndw)){
+	parentDiv.addEventListener('click', (event)=>{
+		// console.log(event.target);
 		modalWndw.style.display = '';
-		body.style.overflow = '';
-	}	
+	  body.style.overflow = '';
+		parentDiv.style.width = '';
+		parentDiv.style.height = '';
+		parentDiv.style.background = '';	
+		modalWndw.stopPropagation;
+		// console.log(event.target);
 	 })
+	 body.addEventListener('click', ()=>{
+	 })
+	 parentDiv.addEventListener('click', ()=>{
+
+	 })
+	 modalWndw.addEventListener('click', (e)=>{
+		 e.stopPropagation();
+	 })
+	 inputOut.addEventListener('blur', checkOut);
+	 inputIn.addEventListener('blur', checkIn);
+	 findBtn.addEventListener('click',(event)=>buttonFunc(event));
 };
+
+function checkOut(){
+		if (inputOut.validity.valueMissing){
+			document.forms[0].reportValidity();
+			inputOut.style.border = '.5px solid red';
+			inputOut.style.boxShadow = '1px 1px 10px rgba(255,0,0, 0.6)';
+			inputOut.setCustomValidity('Поле должно быть заполненым!');
+		} else if (!cyrillicPattern.test(inputOut.value)){
+			inputOut.setCustomValidity('Только русские буквы!');
+			document.forms[0].reportValidity();
+			inputOut.style.border = '.5px solid red';
+			inputOut.style.boxShadow = '1px 1px 10px rgba(255,0,0, 0.2)';
+		}
+		else{
+			inputOut.style.border = '.5px solid green';
+			inputOut.style.boxShadow = '1px 1px 10px rgba(0,255,0, 0.2)';
+			data[`Город отъезда`] = inputOut.value;
+		}
+}
+function checkIn(){
+	if (inputIn.validity.valueMissing){
+		document.forms[0].reportValidity();
+		inputIn.style.border = '.5px solid red';
+		inputIn.style.boxShadow = '1px 1px 10px rgba(255,0,0, 0.2)';
+		inputIn.setCustomValidity('Поле должно быть заполненым!');
+	}	else if (!cyrillicPattern.test(inputIn.value)){
+		inputIn.setCustomValidity('Только русские буквы!');
+		document.forms[0].reportValidity();
+		inputIn.style.border = '.5px solid red';
+		inputIn.style.boxShadow = '1px 1px 10px rgba(255,0,0, 0.2)';
+	}
+	else{
+		inputIn.style.border = '.5px solid green';
+		inputIn.style.boxShadow = '1px 1px 10px rgba(0,255,0, 0.2)';
+		data[`Город приезда`] = inputIn.value;
+	}
+}
+function buttonFunc(event){
+	event.preventDefault();
+	data[`Дата`] = time.value;
+	if (radioTwo.value === 'on') data[`Тип билета`] = 'Взрослый';
+	if (radioOne.value === 'on') data[`Тип билета`] = 'Деткский';
+	console.table(data);
+}	
 
 modalForm();
